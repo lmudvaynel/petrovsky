79d1561e73bd4a6ed397784c34a71f862cb4b0ec
Smirnov Artem <smirnov@balticit.ru>
2014-01-23 18:07:31 +0300

    Add seeds

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,5 @@ class Page < ActiveRecord::Base
 
   validates :slug, presence: true, uniqueness: true
   validates :name, presence: true
-  validates :content, presence: true, html: true
+  validates :content, html: true
 end
--- spec/factories/page.rb
+++ spec/factories/page.rb
@@ -1,10 +1,7 @@
 FactoryGirl.define do
   factory :page do
     name    { Faker::Lorem.words(2).join(' ').capitalize }
+    title   { Faker::Lorem.word }
     content { (Faker::Lorem.words(7).join(' ') + '.').capitalize }
-
-    factory :titled_page do
-      title { Faker::Lorem.word }
-    end
   end
 end
