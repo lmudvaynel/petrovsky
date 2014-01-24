bf7adcc1c43619cab1545e19f521ff7b959c79a7
Smirnov Artem <smirnov@balticit.ru>
2014-01-24 16:13:05 +0300

    Fix seeds

--- db/seeds/pages.rb
+++ db/seeds/pages.rb
@@ -5,8 +5,10 @@ pages_names = {
 }
 Page.reset_column_information
 pages_names.each do |parent_page_name, subpages_names|
-  parent_page = FactoryGirl.create(:page, name: parent_page_name)
+  parent_page = Page.create!(name: parent_page_name,
+                             title: parent_page_name,
+                             content: '')
   parent_page.subpages << subpages_names.map do |subpage_name|
-    FactoryGirl.create(:page, name: subpage_name)
+    Page.create!(name: subpage_name, title: parent_page_name, content: '')
   end
 end
