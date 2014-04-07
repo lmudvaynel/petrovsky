af922c60b12c1938389384ea9b075bc0b4226e98
Artem D <artdemko@gmail.com>
2014-04-07 12:16:50 +0300

    Contacts add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@ class PagesController < ApplicationController
     @page = Page.find_by_slug(:home)
     @message = Message.showed.first
   end
-
   def show
     @page = Page.friendly.find(params[:slug])
+    render params[:slug] if controller_view_exists?(params[:slug])
     @apartments_json = Apartment.all.map(&:to_hash).to_json if params[:slug].to_s == 'floor_plans'
   end
 
@@ -14,5 +14,18 @@ class PagesController < ApplicationController
     apartment = Apartment.find(params[:order][:apartment_id])
     OrderMailer.order_email(user, apartment)
     redirect_to slug_path('floor-plans')
+  end
+  private
+
+  def view_exists?(view)
+    File.exists? Rails.root.join("app", "views", view)
+  end
+
+  def controller_view_exists?(name)
+    view_exists?("#{params[:controller]}/#{name}.html.erb")
+  end
+
+  def layout_exists?(name)
+    view_exists?("layouts/#{name}.html.erb")
   end
 end
