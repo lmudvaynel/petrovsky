38a970c0481efbb1211f55417840925b97dfcb34
Smirnov Artem <smirnov@balticit.ru>
2014-05-05 17:02:56 +0200

    Tiny fixes for floor plans

--- db/seeds/apartments.rb
+++ db/seeds/apartments.rb
@@ -1,3 +1,5 @@
+=begin
+
 floors_count = 6
 aparts_positions = {
   1 => [123, 172],
@@ -9,13 +11,17 @@ Apartment.reset_column_information
 (1..floors_count).each do |floor_number|
   aparts_positions.each do |number, position|
     path = "public/uploads/apartment/image"
-    apartment = Apartment.create! image: open(File.join(Rails.root, path, "#{floor_number}-#{number}.png")),
-                                  dx: position.first,
+    apartment = Apartment.create! dx: position.first,
                                   dy: position.second,
                                   number: number,
                                   floor_number: floor_number,
-                                  sold_out: sold_out
+                                  sold_out: sold_out,
+                                  image: open(File.join(Rails.root, path, "#{floor_number}-#{number}.png")),
+                                  price: rand(1..9) * 100_000,
+                                  area: rand(1..9) * 10
     sold_out = !sold_out
   end
   sold_out = !sold_out
 end
+
+=end
