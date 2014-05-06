c9fa13a9dc552f96c3efacc9e86a6f30eb5ed0a5
Demko <artdemko@gmail.com>
2014-05-06 13:48:51 +0300

    6 sold img

--- db/seeds/apartments.rb
+++ db/seeds/apartments.rb
@@ -8,7 +8,17 @@ aparts_positions = {
   [3, 8] => [1168, 575],
   [3, 9] => [749, 575],
   [3, 10] => [329, 575],
-  [3, 13] => [103, 262]
+  [3, 13] => [103, 262],
+  [6, 1] => [506, 65],
+  [6, 2] => [976, 65],
+  [6, 3] => [1354, 65],
+  [6, 4] => [1543, 65],
+  [6, 5] => [1886, 63],
+  [6, 6] => [1567, 343],
+  [6, 7] => [1174, 343],
+  [6, 8] => [796, 345],
+  [6, 9] => [415, 343],
+  [6, 10] => [213, 61]
 }
 sold_out = false
 
@@ -19,8 +29,8 @@ aparts_positions.each do |number, position|
                                 image: open(File.join(Rails.root, path, "#{number.first}-#{number.second}-sold.png")),
                                 dx: position.first,
                                 dy: position.second,
-                                number: number,
-                                floor_number: floor_number,
+                                number: number.second,
+                                floor_number: number.first,
                                 area: 10,
                                 price: 10,
                                 sold_out: sold_out
